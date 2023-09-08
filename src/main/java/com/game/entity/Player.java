7c11915dfb8dd3ebbec2e0beced265f04dc0048c
Sergei T <tychinkinsergei@yandex.ru>
2023-09-08 19:57:30 +0500

    Added two annotations to the class Player. Added properties and implemented all methods to the class PlayerRepositoryDB

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -6,8 +6,10 @@ import java.util.Date;
 
 @Entity
 @Table(schema = "rpg", name = "player")
+@NamedQuery(name = "player_getAllCount", query = "SELECT COUNT(p) FROM Player p")
 public class Player {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long id;
     @Column(length = 12, nullable = false)
